8acd309acc67a5167c7af289efe08651b3f2a234
roarscarch <2020ugee082@nitjsr.ac.in>
2023-11-22 16:17:09 +0530

    rolled dice

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,6 +3,8 @@ public class SnakeAndLadder{
         System.out.println("Welcome to Snake and Ladder Game");
         int start_position = 0;
         System.out.println("Start Position is: " + start_position);
+        int roll=( int) Math.floor(Math.random()*6)+1;
+        System.out.println("Random Dice Number is: " + roll);
     
     }
 }
