2e1641d170649298ae142ce7ab67f02305d180f0
roarscarch <2020ugee082@nitjsr.ac.in>
2023-11-22 19:32:07 +0530

    2 players features

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,44 +1,73 @@
 public class SnakeAndLadder {
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game");
-        int start_position = 0;
-        int current_position=0;
-        System.out.println("Start Position is: " + start_position);
-     
-        
-        // box === 3 no play
-        // box==1 (play till roll)
-        // box==2 (snake go backwards)
-        int count=0;
-        while(current_position<100){
+    
+        int current_position1 = 0;
+        int current_position2 = 0;
+        int count = 0; // Initialize the count variable
+        boolean flag = false;
+
+        while (current_position1 < 100 && current_position2 < 100) {
             int roll = (int) Math.floor(Math.random() * 6) + 1;
-            int box = (int) Math.floor(Math.random() * 3) + 1;
+            int box = (int) Math.floor(Math.random() * 4); 
 
-        if (box == 1) {
-            current_position = current_position+ roll;
-
-           // System.out.println("Go Ahead " + roll + " steps");
-        } else if (box == 2) {
-
-            current_position = current_position- roll;
-
-            if(current_position < 0){
-                current_position = 0;
+            if (!flag) {// player 1
+                if (box == 1) {// normal
+                    current_position1 = current_position1 + roll;
+                    if (current_position1 > 100) {
+                        current_position1 -= roll;
+                    }
+                    flag = true;
+                } else if (box == 2) {// ladder
+                    current_position1 = current_position1 + roll;
+                    if (current_position1 > 100) {
+                        current_position1 -= roll;
+                    }
+                    flag = false; // Extra chance for ladder
+                } else if (box == 3) {// snake
+                    current_position1 = current_position1 - roll;
+                    if (current_position1 < 0) {
+                        current_position1 = 0;
+                    }
+                    flag = true;
+                } else {// no play
+                    flag = true;
+                }
+            } else {
+                if (box == 1) {// normal
+                    current_position2 = current_position2 + roll;
+                    if (current_position2 > 100) {
+                        current_position2 -= roll;
+                    }
+                    flag = false;
+                } else if (box == 2) {// ladder
+                    current_position2 = current_position2 + roll;
+                    if (current_position2 > 100) {
+                        current_position2 -= roll;
+                    }
+                    flag = true; // Extra chance for ladder
+                } else if (box == 3) {// snake
+                    current_position2 = current_position2 - roll;
+                    if (current_position2 < 0) {
+                        current_position2 = 0;
+                    }
+                    flag = false;
+                } else {// no play
+                    flag = false;
+                }
             }
-            //System.out.println("Snake go backwards " + roll + " steps");
+            count++;
+
+        }
+
+        System.out.println(current_position1 + " " + current_position2);
+
+        if (current_position1 == 100) {
+            System.out.println("Player 1 Wins");
         } else {
-
-             current_position = current_position+0;
-            //System.out.println("No Play");
+            System.out.println("Player 2 Wins");
         }
-        if(current_position >100) {
-            current_position -=roll;
-
-        }
-        System.out.println("Position now :" + current_position);
-        count++;
-    }
-
-        System.out.println("Player Wins Finally ,his position currently is " + current_position+" and after " + count + " rolls");
+        System.out.println("Number of turns: " + count); // Optional: Print the number of turns
     }
 }
