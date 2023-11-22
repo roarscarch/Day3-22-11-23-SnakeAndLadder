1f37ad82309440c2aa996eb532f0a8628d6fbd60
roarscarch <2020ugee082@nitjsr.ac.in>
2023-11-22 16:03:43 +0530

    added start position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,6 +1,8 @@
 public class SnakeAndLadder{
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game");
+        int start_position = 0;
+        System.out.println("Start Position is: " + start_position);
     
     }
 }
