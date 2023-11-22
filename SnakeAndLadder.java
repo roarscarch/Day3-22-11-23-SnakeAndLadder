6cfae4bb9925464b96d74b2236088bd8b18bc63b
roarscarch <2020ugee082@nitjsr.ac.in>
2023-11-22 17:07:27 +0530

    winning logic

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,25 +4,36 @@ public class SnakeAndLadder {
         int start_position = 0;
         int current_position=0;
         System.out.println("Start Position is: " + start_position);
-        int roll = (int) Math.floor(Math.random() * 6) + 1;
-        System.out.println("Random Dice Number is: " + roll);
-
-        int box = (int) Math.floor(Math.random() * 3) + 1;
+     
+        
         // box === 3 no play
         // box==1 (play till roll)
         // box==2 (snake go backwards)
+        int count=0;
+        while(current_position<100){
+            int roll = (int) Math.floor(Math.random() * 6) + 1;
+            int box = (int) Math.floor(Math.random() * 3) + 1;
 
         if (box == 1) {
             current_position = current_position+ roll;
-            System.out.println("Go Ahead " + roll + " steps");
-        } else if (box == 2) {
-            current_position = current_position- roll;
-            System.out.println("Snake go backwards " + roll + " steps");
-        } else {
-             current_position = current_position+0;
-            System.out.println("No Play");
-        }
 
-        System.out.println("Current Position is: " + current_position);
+           // System.out.println("Go Ahead " + roll + " steps");
+        } else if (box == 2) {
+
+            current_position = current_position- roll;
+
+            if(current_position < 0){
+                current_position = 0;
+            }
+            //System.out.println("Snake go backwards " + roll + " steps");
+        } else {
+
+             current_position = current_position+0;
+            //System.out.println("No Play");
+        }
+        count++;
+    }
+
+        System.out.println("Player Wins Finally ,his position currently is " + current_position+" and after " + count + " rolls");
     }
 }
